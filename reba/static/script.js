59845c0622dd062909b279f9b134261af44e3e20
HideyukiWanaka <113268470+HideyukiWanaka@users.noreply.github.com>
2025-04-07 17:46:18 +0900

    script.js を更新

--- reba/static/script.js
+++ reba/static/script.js
@@ -22,7 +22,7 @@ async function initPoseLandmarker() {
     poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
       baseOptions: {
         modelAssetPath:
-          "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task",
+          "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_full/float16/1/pose_landmarker_full.task",
         delegate: "GPU"
       },
       runningMode, // "VIDEO" で初期化された値を使用
